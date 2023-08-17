151ddb70369833e8f5b70a6fdc1f08145285d086
bright_spark <markpassey@btinternet.com>
2023-08-18 00:02:34 +0100

    Removed old code from ChickenCultModClientInit

--- src/main/java/brightspark/chickencultmod/ChickenCultModClientInit.java
+++ src/main/java/brightspark/chickencultmod/ChickenCultModClientInit.java
@@ -2,13 +2,10 @@ package brightspark.chickencultmod;
 
 import brightspark.chickencultmod.entity.ChickenArrowEntityRenderer;
 import brightspark.chickencultmod.geckolib.BasicArmorRenderer;
-import brightspark.chickencultmod.geckolib.BasicItemRenderer;
 import brightspark.chickencultmod.item.BasicGeoArmorItem;
-import brightspark.chickencultmod.item.BasicGeoItem;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import software.bernie.example.client.renderer.armor.GeckoArmorRenderer;
-import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
 
 import static brightspark.chickencultmod.CCItems.*;
 
@@ -17,14 +14,9 @@ public class ChickenCultModClientInit implements ClientModInitializer {
 	public void onInitializeClient() {
 		EntityRendererRegistry.register(CCEntities.CHICKEN_ARROW, ChickenArrowEntityRenderer::new);
 
-//		registerItemRenderer("egg_small", EGG_SMALL);
 		registerArmorRenderer("warrior_chicken_armor",
 			WARRIOR_CHICKEN_HELMET, WARRIOR_CHICKEN_CHESTPLATE, WARRIOR_CHICKEN_LEGGINGS, WARRIOR_CHICKEN_BOOTS
 		);
-	}
-
-	private void registerItemRenderer(String name, BasicGeoItem item) {
-		GeoItemRenderer.registerItemRenderer(item, new BasicItemRenderer<>(name));
 	}
 
 	private void registerArmorRenderer(String name, BasicGeoArmorItem... items) {
